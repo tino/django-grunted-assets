0cc90fa557ee8d4821b28a918be45320d576ae79
Tino de Bruijn <tinodb@gmail.com>
2014-12-23 21:15:25 +0100

    Add inline_asset templatetag

--- grunted_assets/templatetags/grunted_assets.py
+++ grunted_assets/templatetags/grunted_assets.py
@@ -13,6 +13,12 @@ DEFAULT_HTML_TAGS = {
     '.js': '<script src="{}"></script>'
 }
 HTML_TAGS = getattr(settings, 'GRUNTED_ASSETS_HTML_TAGS', DEFAULT_HTML_TAGS)
+DEFAULT_HTML_TAGS_INLINE = {
+    '.css': '<style>{}\n</style>',
+    '.js': '<script>{}\n</script>'
+}
+HTML_TAGS_INLINE = getattr(settings, 'GRUNTED_ASSETS_HTML_TAGS_INLINE',
+                           DEFAULT_HTML_TAGS_INLINE)
 
 DEFAULT_DIRS = {
     True: '.tmp',
@@ -22,8 +28,28 @@ CURRENT_STATIC_DIR = getattr(settings, 'GRUNTED_ASSETS_DIR',
                              DEFAULT_DIRS[settings.DEBUG])
 
 
+def find_asset(filename):
+    # TODO: cache this?
+    filename_re = re.compile(filename, re.IGNORECASE)
+    found_files = []
+    for finder in finders.get_finders():
+        for file in finder.list('xyz'):
+            if file[0].startswith(CURRENT_STATIC_DIR):
+                if filename_re.match(os.path.basename(file[0])):
+                    found_files.append(file)
+    if not found_files:
+        raise IOError('Could not find any file matching {} in {}'.format(
+                      filename, CURRENT_STATIC_DIR))
+    if len(found_files) > 1:
+        raise IOError('Found more than one file matching {} in {}: {}'.format(
+                      filename,
+                      CURRENT_STATIC_DIR,
+                      ', '.join([f[0] for f in found_files])))
+    return found_files[0]
+
+
 @register.simple_tag
-def link_asset(filename):
+def link_asset(filename_re):
     """
     The `{% link_asset "<filename_re>" %}` tag is used to get a specific asset
     from either the development or production asset output folders (by default
@@ -32,28 +58,32 @@ def link_asset(filename):
     `'tail.*\.js'`, matching your `tail.js` in development and
     `tail.f23r0df0se.js` in production.
 
-    It will raise an error when zero or multiple files are found.
+    Raises an error when zero or multiple files are found.
     """
-    # TODO: cache this?
-    filename_re = re.compile(filename, re.IGNORECASE)
-    found_files = []
-    for finder in finders.get_finders():
-        for file in finder.list('xyz'):
-            if file[0].startswith(CURRENT_STATIC_DIR):
-                if filename_re.match(os.path.basename(file[0])):
-                    found_files.append(file[0])
-    if not found_files:
-        raise IOError('Could not find any file matching {} in {}'.format(
-                      filename, CURRENT_STATIC_DIR))
-    if len(found_files) > 1:
-        raise IOError('Found more than one file matching {} in {}: {}'.format(
-                      filename, CURRENT_STATIC_DIR, ', '.join(found_files)))
-    asset = found_files[0]
+    asset = find_asset(filename_re)[0]
     base, ext = os.path.splitext(asset)
     if ext not in HTML_TAGS.keys():
-        raise IOError('Found a file matching {} ({}), but no known html tag '
-                      'found for this extension {}'.format(filename,
-                                                           asset,
-                                                           ext))
+        raise IOError('Found a file matching "{}" ({}), but no known html tag '
+                      'found for this extension "{}"'.format(filename_re,
+                                                             asset,
+                                                             ext))
     return HTML_TAGS[ext].format(static(asset))
+
+
+@register.simple_tag
+def inline_asset(filename_re):
+    """
+    The `{% inline_asset "<filename_re>" %}` tag is used to inline a specific
+    asset. File finding is implemented the same as the `link_asset` tag does.
+
+    Raises an error when zero or multiple files are found.
+    """
+    asset, storage = find_asset(filename_re)
+    base, ext = os.path.splitext(asset)
+    if ext not in HTML_TAGS_INLINE.keys():
+        raise IOError('Found a file matching "{}" ({}), but no known inline '
+                      'html tag found for the extension "{}"'.format(filename_re,
+                                                                     asset,
+                                                                     ext))
+    return HTML_TAGS_INLINE[ext].format(storage.open(asset).read())
 
